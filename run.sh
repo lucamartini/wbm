5e8c20791ce18bd956076f833c5455173f57c622
luca <luca.martini82@gmail.com>
2015-07-14 09:58:47 +0200

    better comment in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# you must chose a run number present in runs/*
-# i.e. ./run.sh 251168
+# you must chose a run number present in runs/* and a lumi in E33
+# i.e. ./run.sh 251168 1.
 
 python read_wbm_L1.py $1
 python read_wbm_HLT.py $1
